77eedc21e6b44c22660d529e004c10a20f5a9739
kerolos hanna saad <merna.hanna92@gmail.com>
2019-12-22 23:34:32 +0200

    second commit

--- src/app/pages/categories-details/categories-details.page.ts
+++ src/app/pages/categories-details/categories-details.page.ts
@@ -11,18 +11,22 @@ import { Router, ActivatedRoute } from '@angular/router';
 export class CategoriesDetailsPage implements OnInit {
   results: any;
   products:any = [];
-  data;
+  name:string;
+  data1:any;
 
-  constructor(public categoriesService: CategoriesService, public navCtrl: NavController, private router: Router, private activeRoute: ActivatedRoute) { }
-
-  ngOnInit() {
-    let id = this.activeRoute.snapshot.paramMap.get('id');
-    console.log('id = ',id);
-    this.categoriesService.getDetailsId(4).subscribe(data => {
+  constructor(public categoriesService: CategoriesService, public navCtrl: NavController, private router: Router, private activeRoute: ActivatedRoute) { 
+    this.activeRoute.queryParams.subscribe(params => {
+      this.data1 = params.special;
+      console.log('id: ',this.data1)
+    });
+    this.categoriesService.getDetailsId(this.data1).subscribe(data => {
       this.results = data;
+      this.name = this.results.name;
       this.products = this.results.products;
       console.log(this.products);
     })
   }
+
+  ngOnInit() {}
 
 }
--- src/app/pages/orginal/orginal.page.ts
+++ src/app/pages/orginal/orginal.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import { CategoriesService } from '../../services/categories.service';
+import { NavController } from '@ionic/angular';
+import { Router, ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-orginal',
   templateUrl: './orginal.page.html',
@@ -7,9 +9,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrginalPage implements OnInit {
 
-  constructor() { }
+  results: any = [];
+  data;
+  constructor(public categoriesService: CategoriesService, public navCtrl: NavController, private router: Router, private activeRoute: ActivatedRoute) {
+    this.appear();
+  }
 
-  ngOnInit() {
+  ngOnInit() { }
+
+
+  //after load the page 
+  ionViewDidLoad() {
+    console.log("work");
+    this.appear();
+  }
+
+  appear() {
+    console.log("hello");
+    this.categoriesService.getDetails().subscribe(data => {
+      this.results = data;
+      console.log(this.results);
+    })
+  }
+
+  goToDetails(id) {
+    let ids = {
+      queryParams:{
+        special: id
+      }
+    }
+    this.router.navigate(['products-details'],ids);
   }
 
 }
+
